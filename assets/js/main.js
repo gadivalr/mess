a61ac8cc541fd3fbe4048d6cbae4dc0247a776c2
Gadi <126745302+gadivalr@users.noreply.github.com>
2023-06-12 14:46:52 -0400

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,4 +1,4 @@
-(() => {
+ (() => {
   // Theme switch
   const body = document.body;
   const lamp = document.getElementById("mode");
@@ -12,14 +12,6 @@
       body.setAttribute("data-theme", "dark");
     } else {
       initTheme(state);
-    }
-
-    // Actualizar Disqus después de cambiar el tema
-    if (typeof DISQUS !== "undefined") {
-      DISQUS.reset({
-        reload: true,
-        config: disqus_config
-      });
     }
   };
 
@@ -37,17 +29,16 @@
       : area.classList.remove("blurry");
   });
 })();
-
 // Obtén la casilla de verificación del menú
-var menuTrigger = document.getElementById('menu-trigger');
+  var menuTrigger = document.getElementById('menu-trigger');
 
-// Obtén todos los enlaces del menú
-var menuLinks = document.querySelectorAll('.menu-link');
+  // Obtén todos los enlaces del menú
+  var menuLinks = document.querySelectorAll('.menu-link');
 
-// Agrega un controlador de eventos a cada enlace del menú
-menuLinks.forEach(function(link) {
-  link.addEventListener('click', function() {
-    // Desmarca la casilla de verificación del menú
-    menuTrigger.checked = false;
+  // Agrega un controlador de eventos a cada enlace del menú
+  menuLinks.forEach(function(link) {
+    link.addEventListener('click', function() {
+      // Desmarca la casilla de verificación del menú
+      menuTrigger.checked = false;
+    });
   });
-});

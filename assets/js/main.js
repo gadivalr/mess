7dcfe991b3bf92d6dea11d15269c6a538f889a2c
Gadi <126745302+gadivalr@users.noreply.github.com>
2023-06-12 15:52:07 -0400

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,4 +1,5 @@
- (() => {
+
+(() => {
   // Theme switch
   const body = document.body;
   const lamp = document.getElementById("mode");
@@ -13,6 +14,9 @@
     } else {
       initTheme(state);
     }
+
+    // Actualizar Disqus después de cambiar el tema
+    
   };
 
   lamp.addEventListener("click", () =>
@@ -29,16 +33,18 @@
       : area.classList.remove("blurry");
   });
 })();
+
 // Obtén la casilla de verificación del menú
-  var menuTrigger = document.getElementById('menu-trigger');
+var menuTrigger = document.getElementById('menu-trigger');
 
-  // Obtén todos los enlaces del menú
-  var menuLinks = document.querySelectorAll('.menu-link');
+// Obtén todos los enlaces del menú
+var menuLinks = document.querySelectorAll('.menu-link');
 
-  // Agrega un controlador de eventos a cada enlace del menú
-  menuLinks.forEach(function(link) {
-    link.addEventListener('click', function() {
-      // Desmarca la casilla de verificación del menú
-      menuTrigger.checked = false;
-    });
+// Agrega un controlador de eventos a cada enlace del menú
+menuLinks.forEach(function(link) {
+  link.addEventListener('click', function() {
+    // Desmarca la casilla de verificación del menú
+    menuTrigger.checked = false;
   });
+});
+
